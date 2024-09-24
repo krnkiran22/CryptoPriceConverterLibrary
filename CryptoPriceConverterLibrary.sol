a9c5005d6f261a8a88031d0ea9f66e8a83860a6d
krnkiran22 <145308198+krnkiran22@users.noreply.github.com>
2024-09-24 19:57:44 +0530

    Update and rename CryptoConverterLibrary.sol to CryptoPriceConverterLibrary.sol

--- CryptoPriceConverterLibrary.sol
+++ CryptoPriceConverterLibrary.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-library CryptoConverterLibrary {
+library CryptoPriceConverterLibrary {
     // Function to convert any crypto amount to another using Chainlink price feeds
     function convertCrypto(
         uint256 amount, // Amount in the source crypto's smallest unit
